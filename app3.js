178e653d73120a24fbb0dd626980cc46b1e8b727
Benjamin Rojo <benjaminrsalgado@gmail.com>
2024-04-19 14:25:55 -0600

    i did a for loop in node environment

--- app3.js
+++ app3.js
@@ -1,1 +1,13 @@
-console.log(process);
+//console.log(process.argv[2]);
+//console.log(process.argv[3]);
+
+//console.log(process.argv);
+
+//{node, app.js3, 6, 7} //argumentos
+// 0,    1,      2, 3   //indices
+
+for ( let i = 2; i < process.argv.length; i++){
+  console.log(process.argv[i]);
+}
+
+//console.log(process.memoryUsage());
