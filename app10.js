9476f79eb7d9daf5fb71ce0ed49cfe314cecade3
Benjamin Rojo <benjaminrsalgado@gmail.com>
2024-04-25 13:35:16 -0600

    i add some functions that convert into syncronas

--- app10.js
+++ app10.js
@@ -1,10 +1,45 @@
 const fs = require ('fs');
 
-fs.readFile ('inde.html', 'utf-8', (err, contenido) => {
-    if (err){
+// Leer un archivo aqui estoy haciendolo solo con Sync
+const archivo = fs.readFileSync ('inde.html', 'utf-8');
+
+console.log(archivo)
+
+// cambia el nombre de un archivo
+fs.rename('index.html', 'main.html', (err) => {
+    if(err){
         throw err;
     }else{
-        console.log(contenido);
+    console.log('Nombre cambiado exitosamente');
+}
+})
+
+// Agregar contenido al fina de un archivo 
+
+fs.appendFile('index.html', '<p> Hola <p>', (err) => {
+    if(err){
+        throw err;
+    }else{
+        console.log('archivo actualizado')
     }
 });
+
+//Remplazar todo el contenido del archivo
+
+fs.writeFile('index.html', 'contenido nuevo', (err) =>{
+    if(err){
+        throw err;
+}
+    console.log('contenido remplazado exitosamente');
+});
+
+//Eliminar archivos
+fs.unlink('main.html', (err) => {
+    if (err){
+    throw err;
+    }
+    console.log('archivo eliminado')
+} )
+
+
 
