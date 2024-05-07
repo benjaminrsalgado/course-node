321992ab488eaa01440ca50d35f85db3840e594e
Benjamin Rojo <benjaminrsalgado@gmail.com>
2024-05-07 14:01:19 -0600

    i did convertion

---
+++ JSON/json-demo.js
@@ -0,0 +1,13 @@
+let infoCurso ={
+    "titulo": "Aprende Node.js",
+    "numVistas":45642,
+    "numLikes":21123,
+    "temas":[
+    "java script",
+    "node.js"
+    ],
+    "es publico": true
+
+}
+let infoCursoJSON = JSON.stringify(infoCurso);
+console.log(infoCursoJSON);
