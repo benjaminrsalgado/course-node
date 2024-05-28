69dfafa9701f569cb223c0cbd8414f32ab6b15fa
Benjamin Rojo <benjaminrsalgado@gmail.com>
2024-05-28 09:43:37 -0600

    i did promises

--- Promesa/app2.js
+++ Promesa/app2.js
@@ -10,15 +10,17 @@ const miPedidoDePizza = new Promise ((resolve,reject) => {
         }
     }, 3000);
 }); 
+
+
 //Primera manera de hacerlo
-const manejarPedido = (mensajeDeConfirmacion) => {
+const manejarPedido1 = (mensajeDeConfirmacion) => {
     console.log(mensajeDeConfirmacion)
 }
 const rechazarPedido = (mensajeDeRechazo) =>{
     console.log(mensajeDeRechazo)
 }
 
-miPedidoDePizza.then(manejarPedido, rechazarPedido);
+miPedidoDePizza.then(manejarPedido1, rechazarPedido);
 
 //Segunda manera de hacerlo Asi se hace si se quiere juntar todo el valor en un codigo 
 miPedidoDePizza
@@ -29,3 +31,23 @@ miPedidoDePizza
     });
 
 //Otra manera de hacerlo es con catch()
+
+miPedidoDePizza
+    .then((mensajeDeConfirmacion) => {
+        console.log(mensajeDeConfirmacion);
+    })
+    .catch((mensajeDeRechazo) => {
+        console.log(mensajeDeRechazo);
+    });
+    
+// Utilizar .catch() solamente escribiendo el nombre de dos funciones 
+
+const manejarPedido = (mensajeDeConfirmacion) => {
+    console.log(mensajeDeConfirmacion);
+}
+
+const manejarRechazo = (mensajeDeError) => {
+    console.log(mensajeDeError);
+}
+
+miPedidoDePizza.then(manejarPedido).catch(manejarRechazo);
